1bc909c90d3e5cfd6db8a0f985a2996f1ee87328
Yusup-beep <yusupyedyyew@gmail.com>
2024-02-15 02:17:18 +0500

    config changes

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,11 +5,12 @@ export default registerAs(
 	'database',
 	(): TypeOrmModuleOptions => ({
 		type: 'postgres',
-		host: process.env.DB_HOST,
-		port: +process.env.DB_PORT,
-		username: process.env.DB_USERNAME,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME,
+		// host: process.env.DB_HOST,
+		// port: +process.env.DB_PORT,
+		// username: process.env.DB_USERNAME,
+		// password: process.env.DB_PASSWORD,
+		// database: process.env.DB_NAME,
+		url: process.env.DB_URL,
 		autoLoadEntities: true,
 		synchronize: true
 	})
